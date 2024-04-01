eee3e148524a710b1dc3e3dcbab5f4da0268cc9f
akhattaway <katreecehattaway@gmail.com>
2024-03-31 23:15:49 -0500

    colors scale markers working with pop ups, legend makes the markers stop displaying, need to figure out legend and update Read Me.

--- Starter_Code_Mod_15/static/js/logic.js
+++ Starter_Code_Mod_15/static/js/logic.js
@@ -1,3 +1,11 @@
+// define a color function outside the data processing loop
+function getColor(magnitude) {
+    return magnitude > 6.5 ? '#d73027' :
+           magnitude > 5.5 ? '#fc8d59' :
+           magnitude > 4.5 ? '#fee08b' :
+                             '#ffffbf'; // Default color for magnitudes less than or equal to 4.5
+}
+
 // Initialize the map on the "map" div reference in the html file with a given center and zoom
 let myMap = L.map('map', {
     center: [0, 0], //center of earth 
@@ -5,47 +13,58 @@ let myMap = L.map('map', {
 });
 
 // Add a tile layer to the map, natural first step after initialize your map
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 let centerMarker = L.marker([0,0]).addTo(myMap);
-
 // Add a marker to the center
-centerMarker.bindPopup("Hello from Center of the Earth world!");
+centerMarker.bindPopup("myMap Center [0,0]");
 
 // URL for the earthquake data
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 // Fetch the earthquake data
 fetch(queryUrl).then(function(response) {
-    // Parse the response to JSON
     return response.json();
 }).then(function(data) {
-    // Once we have the data, we can start plotting it on the map
+    // Earthquake data plotting...
     data.features.forEach(function(earthquake) {
-        // Extract the coordinates, magnitude, and depth
         let coordinates = earthquake.geometry.coordinates;
         let magnitude = earthquake.properties.mag;
         let depth = coordinates[2]; // The depth is the 3rd item in the coordinates array
         
         // Create a marker and add it to the map
-        // Note: You'll need to modify this to change the marker's size and color
         let marker = L.circleMarker([coordinates[1], coordinates[0]], {
-            radius: magnitude * 5, // Example scaling, you'll need to refine this
-            fillColor: "purple", // Example color, you'll need a function to change based on depth
-            color: "black",
+            radius: magnitude * 5, // Example scaling, you might adjust this
+            fillColor: getColor(magnitude), // You can also dynamically change this based on conditions
+            color: "white",
             weight: 1,
             opacity: 1,
-            fillOpacity: 0.6
+            fillOpacity: 1 // Set fillOpacity based on magnitude
         }).addTo(myMap);
-
-        // Add a popup to the marker
-        marker.bindPopup(`<h3>${earthquake.properties.place}</h3><hr><p>${
-            new Date(earthquake.properties.time)}</p><p>Magnitude: ${
-                magnitude}</p><p>Depth: ${depth} km</p>`);
-    });
     
+        // Add a popup to the marker
+        marker.bindPopup(`<h3>${earthquake.properties.place}</h3><hr><p>${new Date(earthquake.properties.time)}</p><p>Magnitude: ${magnitude}</p><p>Depth: ${depth} km</p>`);
+    });    
 });
 
+ var legend = L.control({position: 'bottomright'});
+
+// legend.onAdd = function (map) {
+  //  var div = L.DomUtil.create('div', 'info legend'),
+   //     grades = [0, 4.5, 5.5, 6.5, 7.5], // These are the breaks in your scale
+   //     labels = [];
+
+    // Add min & max
+   // div.innerHTML = '<div><b>Magnitude</b></div>';
+    // loop through our intervals and generate a label with a colored square for each interval
+   // for (var i = 0; i < grades.length; i++) {
+      //  div.innerHTML +=
+        //    '<div style="background:' + getColor(grades[i]) + '; height:20px; margin: 2px 0;">&nbsp;</div>' +
+          //  '<div style="text-align: center">' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] : '+') + '</div>';
+   // }
+   // return div;
+//};
+
+legend.addTo(myMap);
